8f485b9505f82657068dde7d537862e2079f362b
paulandreid <49744892+paulandreid@users.noreply.github.com>
2022-10-02 19:51:33 +0300

    end current task logic

--- main/main.py
+++ main/main.py
@@ -1,6 +1,8 @@
 from asyncio.windows_events import NULL
+from ctypes import sizeof
 from datetime import date, time, datetime
 import json
+from operator import truediv
 
 #description = input()
 #account = input()
@@ -20,18 +22,22 @@ def toHours(dateStart, dateEnd):
     seconds = timeDelta.total_seconds()
     return round(seconds / 3600, 2)
 
+def newTask():
+    f = open("entries.txt", "a")   
+    description = input("Description: ")
+    f.write( str(datetime.now()) + ',' + description)
+    f.close()
+    
+
 def start():
     description = input()
+
+    if task_still_runnig() == False:
+        read_last_entry()
     startTime = str(datetime.now())
-    dictJson = {
-        "description" : description,
-        "start" : startTime
-    }
 
-    print(json.dumps(dictJson))
-
-    f = open("entries.txt", "a")
-    f.write(startTime + "," + description) #
+    f = open("entries.txt", "a", newline='')
+    f.write('\n' + startTime + "," + description) #
     f.close()
 
 
@@ -47,14 +53,31 @@ def read_last_entry():
     duration = datetime.now() - datetime.strptime(startTime, "%Y-%m-%d %H:%M:%S.%f") 
     print(round(duration.seconds / 60,2))
     f = open("entries.txt", "a", newline='')    
-    f.write(","+str(round(duration.seconds / 60 ,2))+"\n")
+    f.write(","+str(round(duration.seconds / 60 ,2)))
+
+def task_still_runnig():
+    with open("entries.txt") as f:
+        for line in f:
+            pass
+        last_line = line
+        f.close()
+        last_line = last_line.split(",")
+        if len(last_line) == 2 :
+            print("current task: " + last_line[1])
+            return True
+    return False
+
 
 
 
 if __name__ == "__main__":  
     #start()
-    decision = input()
+    decision = input("1: End current task \n" +
+                     "2: Start new task ")
     if decision == '1':
-        read_last_entry()
+        if task_still_runnig()==True:
+            read_last_entry()
+        else:
+            print("no task running")
     else:
         start()
